6fb88c2dff7d703cb621d423c34fc5170c062198
Hiroto Shioi <shioihigg@gmail.com>
2025-10-18 16:26:12 +0900

    Tanstack Query

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,22 @@
 import { createRouter } from '@tanstack/react-router'
+import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
 import { routeTree } from './routeTree.gen'
 import { NotFound } from './components/NotFound'
+import { queryClient } from './lib/queryClient'
 
 export function getRouter() {
   const router = createRouter({
     routeTree,
+    context: {
+      queryClient,
+    },
     defaultPreload: 'intent',
     defaultNotFoundComponent: () => <NotFound />,
     scrollRestoration: true,
+  })
+  setupRouterSsrQueryIntegration({
+    router,
+    queryClient,
   })
 
   return router
